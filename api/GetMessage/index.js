c130bc94b1e15ea968f5934d5d99c1e3ad5260df
kingkino <kingkino@gmail.com>
2020-05-21 22:08:20 +0900

    日次を入れれるように変更

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,7 +1,15 @@
+var now = new Date();
 module.exports = async function (context, req) {
+    var Year = now.getFullYear();
+    var Month = now.getMonth() + 1;
+    var Date = now.getDate();
+    var Hour = now.getHours();
+    var Min = now.getMinutes();
+    var Sec = now.getSeconds();
+    var result = Year + "年" + Month + "月" + Date + "日" + Hour + ":" + Min + ":" + Sec;
     context.res = {
         body: {
-            text: "Hello from the API"
+            text: "今何時？" + result
         }
     };
 };
