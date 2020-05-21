2b333e73d41f9ff6e82d6c85504f29d5e1183bd3
kingkino <kingkino@gmail.com>
2020-05-21 23:13:47 +0900

    日付コントロールを追加

--- api/GetDateTime/index.js
+++ api/GetDateTime/index.js
@@ -1,12 +1,17 @@
-var now = new Date();
+const {
+    DateTime
+} = require('luxon');
+let now = new Date();
 module.exports = async function (context, req) {
-    var Year = now.getFullYear();
-    var Month = now.getMonth() + 1;
-    var Date = now.getDate();
-    var Hour = now.getHours();
-    var Min = now.getMinutes();
-    var Sec = now.getSeconds();
-    var result = Year + "年" + Month + "月" + Date + "日" + Hour + ":" + Min + ":" + Sec;
+    let dt = DateTime.local().setZone('Asia/Tokyo');
+
+    let Year = dt.year;
+    let Month = dt.month;
+    let Date = dt.day;
+    let Hour = dt.hour;
+    let Min = dt.minute;
+    let Sec = dt.second;
+    let result = Year + "年" + Month + "月" + Date + "日" + Hour + ":" + Min + ":" + Sec;
     context.res = {
         body: {
             text: "今何時？" + result
